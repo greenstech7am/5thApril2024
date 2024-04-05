164cf7914af7b34563adbb6c2b874cfc18f411e6
greenstech7am <greenstechtask@gmail.com>
2024-04-05 09:50:51 +0530

    JDBC Project

---
+++ src/org/tcs/Sample.java
@@ -0,0 +1,68 @@
+package org.tcs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Sample {
+	
+	
+	public static void main(String[] args) {
+		
+		Connection con=null;
+		
+		// 1. Load the driver
+	try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		//2.Connect to database
+		 con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "admin");
+		
+		//3.Write a SQL query
+		String query = "select * from student";
+		
+		//4. Prepare the statement
+		PreparedStatement ps = con.prepareStatement(query);
+		
+		//5. execute query
+		ResultSet rs = ps.executeQuery();
+		
+		//6. Iterate the results
+		while (rs.next()) {
+			
+			int stuId = rs.getInt("sid");
+			System.out.println(stuId);
+			String stuName = rs.getString("sname");
+			System.out.println(stuName);
+				
+		}
+		
+		
+		
+	} catch (ClassNotFoundException | SQLException e) {
+		
+		e.printStackTrace();
+	}
+	finally {
+		
+		//7. Close the db connection
+		try {
+			con.close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/org/testing/Employee.java
@@ -0,0 +1,23 @@
+package org.testing;
+
+public class Employee extends Exception{
+	
+	
+
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+		
+	
+		
+		
+		
+		
+	
+
+	}
+
+}
